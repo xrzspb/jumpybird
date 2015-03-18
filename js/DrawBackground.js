74a542698df63b23b2a5d9ba10c612ef05981063
xrzspb <mengyan.fudan@gmail.com>
2015-03-17 23:24:21 -0700

    Testing a moving box

--- js/DrawBackground.js
+++ js/DrawBackground.js
@@ -1,7 +1,15 @@
 /**
  * Created by mengyan on 3/17/15.
  */
-$(document).ready( function(){
+$(document).ready( function() {
     //TODO: need to draw a continuous running background
-
+    function loop() {
+        $('#box').css({left:0});
+        $('#box').animate ({
+            left: '+=1400'
+        }, 45000, 'linear', function() {
+            loop();
+        });
+    }
+    loop();
 });
