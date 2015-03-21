13717a344840b0102009cdb5837e474b69aa5dcb
xrzspb <mengyan.fudan@gmail.com>
2015-03-20 21:09:40 -0700

    create a series of boxes with animation

--- js/DrawBackground.js
+++ js/DrawBackground.js
@@ -3,13 +3,36 @@
  */
 $(document).ready( function() {
     //TODO: need to draw a continuous running background
-    function loop() {
-        $('#box').css({left:0});
-        $('#box').animate ({
+
+    for (var i = 0; i < 1; i++) {
+        var box = generateNewBox(i);
+        document.getElementById("playing").appendChild(box);
+        loop(i);
+    }
+
+
+    function loop(i) {
+        if (shouldStop(i)) {
+            return;
+        }
+        $('#box'+i).css({left:0});
+        $('#box'+i).animate ({
             left: '+=1400'
-        }, 45000, 'linear', function() {
-            loop();
+        }, 5000, 'linear', function() {
+            loop(i);
         });
     }
-    loop();
 });
+
+function shouldStop(i) {
+    return $('#box'+i).offset.left + ($('#box'+i).outerWidth()) >= $(window).outerWidth();
+}
+
+function generateNewBox(i) {
+    var x = document.createElement("CANVAS");
+    var ctx = x.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(20, 20, 150, 100);
+    x.setAttribute("id", "box"+i);
+    return x;
+}
